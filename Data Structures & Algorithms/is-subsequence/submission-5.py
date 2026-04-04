fda62b454517b094bf4a7401fc2332fe389f1917
ArsalJafri <154714497+ArsalJafri@users.noreply.github.com>
2026-04-04 13:45:08 -0500

    Add: is-subsequence - submission-5

---
+++ Data Structures & Algorithms/is-subsequence/submission-5.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        left = 0 #keeps track of t
+        right = 0 # keeps track of s
+
+        while left < len(t) and right < len(s): 
+            if t[left] == s[right]: #we found a match!
+                left += 1
+                right += 1
+            else:
+                left += 1 # keep t moving
+
+        if len(s) == right:
+            return True
+        else:
+            return False
+            
+        
